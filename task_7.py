defd9fe80bb6002f5101bbe2b391ebf9f1e9bd48
infizstudent <Artem.Kravchuk@infiz.khpi.edu.ua>
2023-05-13 22:30:06 +0300

    I added if __name__ == '__main__':, optimized the use of the list and finding the average score using statistics and itertools, and also removed Cyrillic from the program.

--- task_7.py
+++ task_7.py
@@ -1,3 +1,6 @@
+import statistics
+from itertools import chain
+
 
 class Student:
     def __init__(self, last_name, first_name, grades_dict):
@@ -5,52 +8,49 @@ class Student:
         self.first_name = first_name
         self.grades_dict = grades_dict
 
-
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
-
+        return f'{self.first_name} {self.last_name}'
 
     def semester_average(self, semester):
-        grades = self.grades_dict.get(semester, [])
-        if not grades:
-            return None
-        return sum(grades) / len(grades)
-
+        return statistics.mean(filter(None, self.grades_dict.get(semester))) if self.grades_dict.get(semester) else None
 
     def overall_average(self):
-        all_grades = [grade for grades in self.grades_dict.values() for grade in grades]
-        return sum(all_grades) / len(all_grades)
+        all_grades = chain.from_iterable(self.grades_dict.values())
+        all_grades = (grade for grade in all_grades if grade is not None)
+        return statistics.mean(all_grades)
 
+    def set_semester_grades(self, semester, grades):
+        self.grades_dict[semester] = grades
+
+    def get_semester_grades(self, semester):
+        return self.grades_dict.get(semester, [])
 
     def set_semester_grades(self, semester, grades):
         self.grades_dict[semester] = grades
 
 
-    def get_semester_grades(self, semester):
-        return self.grades_dict.get(semester, [])
+if __name__ == '__main__':
+    grades_dict = {
+        1: [3, 3, 3, 5],
+        2: [3, 3, 4, 5],
+        3: [4, 5, 3, 3],
+        4: [5, 3, 3, 3]
+    }
 
+    student = Student('Kravchuk', 'Artem', grades_dict)
 
-grades_dict = {
-    1: [3, 3, 3, 5],
-    2: [3, 3, 4, 5],
-    3: [4, 5, 3, 3],
-    4: [5, 3, 3, 3]
-}
+    print(f'Student: {student}')
 
-student = Student("Кравчук", "Артем", grades_dict)
+    semester = 2
+    avg = student.semester_average(semester)
+    print(f'Avg grade for semester {semester}: {avg}')
 
-print(f"Студент: {student}")
+    overall_avg = student.overall_average()
+    print(f'Avg grade for all semesters: {overall_avg}')
 
-semester = 2
-avg = student.semester_average(semester)
-print(f"Середній бал за {semester}-й семестр: {avg}")
+    semester = 5
+    grades = [5, 4, 5, 4]
+    student.grades_dict = {**student.grades_dict, **{semester: grades}}
 
-overall_avg = student.overall_average()
-print(f"Середній бал за всі роки навчання: {overall_avg}")
-
-semester = 5
-grades = [5, 4, 5, 4]
-student.set_semester_grades(semester, grades)
-
-semester_grades = student.get_semester_grades(semester)
-print(f"Оцінки за {semester}-й семестр: {semester_grades}")
+    semester_grades = student.get_semester_grades(semester)
+    print(f'Grades for semester {semester}: {semester_grades}')
