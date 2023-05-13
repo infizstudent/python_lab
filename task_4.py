abbe81fb0e094d4156f76006b57bafa7d025cd55
infizstudent <Artem.Kravchuk@infiz.khpi.edu.ua>
2023-05-13 17:57:14 +0300

    made some changes

--- task_4.py
+++ task_4.py
@@ -3,32 +3,29 @@ from utils import print_intro
 
 print_intro()
 
-
 def get_input():
     while True:
-        input_data = input("Enter an integer m or 'exit' to quit: ")
+        input_data = input("Enter an integer input_number or 'exit' to quit: ")
         if input_data.lower() == 'exit':
             sys.exit()
         try:
-            m = int(input_data)
-            return m
+            input_number = int(input_data)
+            return input_number
         except ValueError:
             print("Error: Please enter an integer or 'exit' to quit.")
 
-
-def find_fibonacci(m):
-    if m <= 1:
+def find_fibonacci(input_number):
+    if input_number <= 1:
         return 1
     f1, f2 = 1, 1
     while True:
         fn = f1 + f2
-        if fn > m:
+        if fn > input_number:
             return fn
         f1, f2 = f2, fn
 
-
 if __name__ == '__main__':
     while True:
-        m = get_input()
-        result = find_fibonacci(m)
-        print(f"The first Fibonacci number greater than {m} is {result}.")
+        input_number = get_input()
+        result = find_fibonacci(input_number)
+        print(f'The first Fibonacci number greater than {input_number} is {result}.')
