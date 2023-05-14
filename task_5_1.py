73d98b294ca554df4e70f51b5c470595a4611d6f
infizstudent <Artem.Kravchuk@infiz.khpi.edu.ua>
2023-05-14 17:13:35 +0300

    add task_5_1

--- task_5_1.py
+++ task_5_1.py
@@ -7,8 +7,8 @@ def calculate_series_sum(x, precision):
     sum = 0
     term = 1
     n = 0
-    while abs(term) >= 10 ** (-1 *):
-        term = 2 ** n * math.factorial(n) / n ** n
+    while abs(term) >= 10 ** (-1 * precision):
+        term = (x ** n / n ** n) * math.factorial(n) / math.exp(n)
         sum += term
         n += 1
     return round(sum, precision)
