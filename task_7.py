9c617a3491553e2d1751a8447a6417273e9db99c
infizstudent <Artem.Kravchuk@infiz.khpi.edu.ua>
2023-05-18 18:38:47 +0300

    renamed methods in the class

--- task_7.py
+++ task_7.py
@@ -11,22 +11,18 @@ class Student:
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
 
-    def semester_average(self, semester):
-        return statistics.mean(filter(None, self.grades_dict.get(semester))) if self.grades_dict.get(semester) else None
+    def semester_average(self, grades_semester):
+        return statistics.mean(self.grades_dict[grades_semester]) if grades_semester in self.grades_dict else None
 
-    def overall_average(self):
+    def calculate_overall_average(self):
         all_grades = chain.from_iterable(self.grades_dict.values())
-        all_grades = (grade for grade in all_grades if grade is not None)
-        return statistics.mean(all_grades)
+        return statistics.mean((grade for grade in all_grades))
 
-    def set_semester_grades(self, semester, grades):
-        self.grades_dict[semester] = grades
+    def set_grades_for_semester(self, grades_semester, grades):
+        self.grades_dict[grades_semester] = grades
 
-    def get_semester_grades(self, semester):
-        return self.grades_dict.get(semester, [])
-
-    def set_semester_grades(self, semester, grades):
-        self.grades_dict[semester] = grades
+    def get_grades_for_semester(self, grades_semester):
+        return self.grades_dict.get(grades_semester, [])
 
 
 if __name__ == '__main__':
@@ -41,16 +37,16 @@ if __name__ == '__main__':
 
     print(f'Student: {student}')
 
-    semester = 2
-    avg = student.semester_average(semester)
-    print(f'Avg grade for semester {semester}: {avg}')
+    grades_semester = 2
+    avg = student.semester_average(grades_semester)
+    print(f'Avg grade for semester {grades_semester}: {avg}')
 
-    overall_avg = student.overall_average()
+    overall_avg = student.calculate_overall_average()
     print(f'Avg grade for all semesters: {overall_avg}')
 
-    semester = 5
-    grades = [5, 4, 5, 4]
-    student.grades_dict = {**student.grades_dict, **{semester: grades}}
+    grades_semester = 5
+    new_grades = [5, 4, 5, 4]
+    student.grades_dict = {**student.grades_dict, **{grades_semester: new_grades}}
 
-    semester_grades = student.get_semester_grades(semester)
-    print(f'Grades for semester {semester}: {semester_grades}')
+    semester_grades = student.get_grades_for_semester(grades_semester)
+    print(f'Grades for semester {grades_semester}: {semester_grades}')
